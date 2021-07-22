c2a1f12eabcff70f94a05878c75090e6a0f21dfa
terencechan <terence.kfc@gmail.com>
2021-07-22 22:49:55 +0800

    Added users schema and seedUsers

--- models/user.js
+++ models/user.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  address: { street: String, poscode: Number },
+  handphone: { type: Number, required: true },
+  comments: [String]
 });
 
 const User = mongoose.model("user", userSchema);
