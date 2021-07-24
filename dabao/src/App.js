1eabec093f0e0c5859f9e7ec9c6783aa24ab6e94
terencechan <terence.kfc@gmail.com>
2021-07-24 12:12:41 +0800

    Setup seed users, users CRUD API

--- dabao/src/App.js
+++ dabao/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>TEST</h1>
+      <h1>TEST FROM REACT</h1>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const session = require("express-session");
 const methodOverride = require("method-override");
+const bcrypt = require("bcrypt")
+const cors = require("cors")
 
 require("dotenv").config();
 const PORT = process.env.PORT;
@@ -20,11 +22,40 @@ app.use(
   app.use(methodOverride("_method"));
   app.use(express.static("public"));
 
+  app.use(cors()) 
+  app.use(express.json());
+
+// Controllers
+const usersController = require('./controllers/users.js');
+app.use('/users', usersController);
+
+
+const seed = require('./models/users.seed.js');
+const User = require('./models/users.model.js');
+
+// Seed Routes - Use once to populate DB
+app.get('/seedUsers', (req, res) => {
+  seed.forEach((user) => {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10));
+  });
+  User.create(seed, (err, createdUsers) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else {
+    res.status(200).json(createdUsers);
+  }
+    console.log(createdUsers);
+});
+
+})
 
 //Routes
 app.get("/", (req, res) => {
-    res.send("TEST");
+    res.send("TEST FROM EXPRESS");
   });
+
+
+
   app.listen(PORT, () => {
     console.log("Listening on the port", PORT);
   });
