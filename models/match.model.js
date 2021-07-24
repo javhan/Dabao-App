a9efb09db80f5505d601c8f5cb0e18a68de6414f
WenHan <wenhantan94@gmail.com>
2021-07-24 17:31:27 +0800

    Updated Models and seed data

--- models/match.model.js
+++ models/match.model.js
@@ -1,7 +1,27 @@
 const mongoose = require("mongoose");
 
 const matchSchema = new mongoose.Schema({
-
+  timePosted: Date,
+  DBER: { type: Schema.Types.ObjectId, ref: "User" },
+  maxOrders: { type: Number, max: 2 },
+  Orders: [
+    {
+      DBEE: { type: Schema.Types.ObjectId, ref: "User" },
+      isCompleted: Boolean,
+      remarks: String,
+      dishOrdered: { type: Schema.Types.ObjectId, ref: "Shop" },
+      messages: [
+        {
+          message: String,
+          timePosted: Date,
+          user: { type: Schema.Types.ObjectId, ref: "User" },
+        },
+      ],
+    },
+  ],
+  pickupLocation: { type: Schema.Types.ObjectId, ref: "User" },
+  timeAtPickUp: Date,
+  orderLocation: { type: Schema.Types.ObjectId, ref: "Shop" },
 });
 
 const Match = mongoose.model("Match", matchSchema);
