8013d3dd807459c2077268fea684b35eab247d4a
Darylaw93 <darylaw93@gmail.com>
2021-07-26 22:25:43 +0800

    Fixed questionnaire & board

--- dabao/src/components/Board.js
+++ dabao/src/components/Board.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Nav from "./Nav";
 import { Link } from "react-router-dom";
-
 const Board = () => {
+    const [list, setList] = useState([])
+    useEffect(() => {
+    fetch("/match")
+    .then((res)=> {
+        if 
+        (res.ok) {
+            return res.json()
+        }
+    })
+    .then((data)=> {
+        if (data === undefined){
+            return "Loading.."
+        } else {
+            return setList(data)
+        }
+    })
+}, []);
+
+console.log(list)
+
+  const row = list.map((data) => {
+       return (
+        <tr>
+           <td>{data.orderLocation?.street}</td>
+           <td>{data.pickupLocation?.street}</td>
+           <td>{data.timeAtPickUp}</td>
+        </tr>
+           );
+   })
+
+   console.log("Row", row)
+
   return (
     <Nav>
       <div className="box">
-        <h1>Board</h1>
+          <table>
+              <thead>
+              <tr>
+                  <th>Food</th>
+                  <th>Pick Up Location</th>
+                  <th>Pick Up Time</th>
+              </tr>
+              </thead>
+              <tbody>
+                  {row}
+            </tbody>
+          </table>
       </div>
       <div>
         <Link to="/dashboard">To Dashboard</Link>
--- dabao/src/components/Question.js
+++ dabao/src/components/Question.js
@@ -1,80 +1,60 @@
-import React from "react"
-import Nav from "./Nav"
-
+import React from "react";
+import Nav from "./Nav";
+import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const Question = () => {
-    const history = useHistory();
+  const history = useHistory();
   const handleSubmit = (event) => {
     fetch("/match", {
       method: "POST",
       body: JSON.stringify({
         maxOrders: event.target.maxOrders.value,
         pickupLocation: {
-          street: event.target.street.value,
-          postCode: event.target.postCodeOrder.value,
+          street: event.target.pickup.value,
         },
         timeAtPickUp: event.target.timeAtPickUp.value,
+        orderLocation: { street: event.target.eating.value },
+
       }),
       headers: {
         "Content-Type": "application/json",
       },
-    })
-      .then((res) => {
-        if (res.ok) {
-          res.json();
-        }
-      });
-    history.push("/dashboard")
+    }).then((res) => {
+      if (res.ok) {
+        res.json();
+      }
+    });
+    history.push("/dashboard");
   };
   return (
     <Nav>
       <div className="box">
         <h1>DBer Questionnaire</h1>
         <form onSubmit={handleSubmit}>
-            <fieldset>
-        <legend>Where are you DB-ing?</legend>
-          <input
-            type="text"
-            name="street"
-          />
-           </fieldset>
-          <br/>
           <fieldset>
-        <legend>Postal Code of DB place</legend>
-          <input
-            type="text"
-            name="postCodeOrder"/>
+            <legend>Where are you Eating?</legend>
+            <input type="text" name="eating" />
           </fieldset>
-          <br/>
+          <br />
           <fieldset>
-        <legend>Pick-Up Location</legend>
-          <input
-            type="text"
-            name="postCodePickUp"
-            placeholder="Postal Code"
-          />
+            <legend>Where to Pick-up?</legend>
+            <input type="text" name="pickup" />
           </fieldset>
-          <br/>
+          <br />
           <fieldset>
-        <legend>Pick-Up Time!</legend>
-          <input
-            type="datetime-local"
-            name="timeAtPickUp"
-          />
+            <legend>Pick-Up Time!</legend>
+            <input type="datetime-local" name="timeAtPickUp" />
           </fieldset>
-          <br/>
+          <br />
           <fieldset>
-        <legend>No. of Orders</legend>
-          <select
-            type="range"
-            name="maxOrders"
-          >
-        <option value="1">1</option>
-        <option value="2">2</option>
-        </select>
+            <legend>No. of Orders</legend>
+            <select type="range" name="maxOrders">
+              <option value="1">1</option>
+              <option value="2">2</option>
+            </select>
           </fieldset>
-          <br/>
+          <br />
           <input type="submit" value="Confirm!" />
         </form>
       </div>
