87f281dcb6124e89b2252073a0e1585ed89a331d
terencechan <terence.kfc@gmail.com>
2021-07-26 12:24:39 +0800

    Setup create new user from front end to back

--- dabao/src/components/SignUp.js
+++ dabao/src/components/SignUp.js
@@ -1,11 +1,64 @@
 import * as React from "react";
+import {useState} from "react";
 import Nav from "./Nav"
 const SignUp = () => {
+  const [isConfirmPwdSame, setIsConfirmPwdSame] = useState(true)
+
+  // need to make sure password and confirm password is same
+  // the new user API is post method with /users route
+  const handleRegisterUser = (event) => {
+    event.preventDefault();
+    if(event.target.password.value !== event.target.confirmPassword.value) {
+      setIsConfirmPwdSame(false);
+      return;
+    }
+    
+    fetch("http://localhost:4000/users", {
+      method: "POST",
+      body: JSON.stringify({ 
+        username: event.target.username.value,   
+        password: event.target.password.value,
+        email: event.target.email.value,
+
+      }),
+      headers: {
+      "Content-Type": "application/json",
+      },
+    })
+    .then((res) => { 
+      res.json()
+      console.log(res)
+    })
+      .then((resJson) => {
+        // props.handleAddHoliday(resJson);
+        console.log(resJson)
+      })
+      .catch((error) => console.error({ Error: error }));
+
+  }
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   fetch("/holidays", {
+  //     method: "POST",
+  //     body: JSON.stringify({ name: event.target.name.value }),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   })
+  //     .then((res) => res.json())
+  //     .then((resJson) => {
+  //       props.handleAddHoliday(resJson);
+  //     })
+  //     .catch((error) => console.error({ Error: error }));
+  // }
+
+
   return (
       <Nav>
       <h1>Sign Up!</h1>
     <div className="SignUpForm">
-      <form>
+      <form onSubmit={handleRegisterUser}>
         <label>
           Username:
           <br></br>
@@ -20,8 +73,8 @@ const SignUp = () => {
           Confirm Password:
           <br></br>
           <input
-            type="password"
-            name="password"
+            type="confirmPassword"
+            name="confirmPassword"
             placeholder="Confirm Password"
           />
         </label>
@@ -32,7 +85,8 @@ const SignUp = () => {
         </label>
         <br></br>
         <input type="submit" value="Register" />
-      </form>
+      </form><br /><br />
+      {!isConfirmPwdSame && <h3>Password Difference</h3>}
     </div>
     </Nav>
   );
--- models/users.model.js
+++ models/users.model.js
@@ -3,15 +3,15 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique : true },
   password: { type: String, required: true },
-  address: { 
-      street : { type : String, required : true},
-      postCode : { type : Number, required : true }
-  },
+  // address: { 
+  //     street : { type : String, required : true},
+  //     postCode : { type : Number, required : true }
+  // },
   contact : {
-      hp : { type : Number, required : true },
+      // hp : { type : Number, required : true },
       email : { type: String, required: true, unique : true }
-  },
-  isDabao : { type : String, default : "DBee" }
+  // },
+  // isDabao : { type : String, default : "DBee" }
 },{ timestamps:true });
 
 const User = mongoose.model("user", userSchema);
