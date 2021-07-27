8896547ebdb9b013a714f6abda8917cafe8888e0
terencechan <terence.kfc@gmail.com>
2021-07-27 09:01:00 +0800

    Update DBer Board

--- controllers/match.js
+++ controllers/match.js
@@ -27,19 +27,21 @@ router.get("/id/:id", (req, res) => {
   });
 });
 
-// Get all users under same poscode
+// Get all match under same poscode
 // curl http://localhost:<PORT>/users/poscode/<poscode>
-router.get("/postcode/:postcode", (req, res) => {
-  console.log(req.params.postcode)
-  
-  Match.find({ 'pickupLocation.postCode':req.params.postcode }, (err, matchesFound) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-    }
-    // console.log(matchesFound)
-    res.status(200).json(matchesFound);
-  });
+router.get("/postcode/:postcode", async (req, res) => {
+  // console.log(req.params.postcode)
+  const match = await Match.find({ 'pickupLocation.postCode':req.params.postcode }).populate('DBER')
+  res.status(200).json(match)
+  // Match.find({ 'pickupLocation.postCode':req.params.postcode }, (err, matchesFound) => {
+  //   if (err) {
+  //     res.status(400).json({ error: err.message });
+  //   }
+  //   // console.log(matchesFound)
+  //   res.status(200).json(matchesFound);
+  // });
 });
+// .populate({path: "DBER",model:"User"})
 
 // Create a new Match
 router.post("/", (req, res) => {
@@ -63,11 +65,11 @@ router.delete("/:id", (req, res) => {
   });
 });
 
-//* Update a Match details 
+//* Insert DBEE
 // curl -X PUT -H "Content-Type: application/json" -d
 // '{"usermame":"I updated this"}' http://localhost:<PORT>/match/<id>
-router.put("/:orderId", (req, res) => {
-//  console.log("order id", req.params.orderId)
+router.put("/insert/:orderId", (req, res) => {
+ console.log("order id", req.params.orderId)
   req.body.DBEE = new mongoose.mongo.ObjectId(req.body.DBEE)
   Match.findByIdAndUpdate(
     req.params.orderId,
@@ -81,6 +83,24 @@ router.put("/:orderId", (req, res) => {
     }
   );
 });
+
+// Remove DBEE
+router.put("/remove/:orderId/:dbee", (req, res) => {
+  console.log("order id", req.params.orderId)
+  console.log("order id", req.params.dbee)
+  //  req.body.DBEE = new mongoose.mongo.ObjectId(req.body.DBEE)
+   Match.findByIdAndUpdate(
+     req.params.orderId,
+     {$pull: {Orders: {DBEE:req.params.dbee}}},
+     { new: true },
+     (err, updatedMatch) => {
+       if (err) {
+         res.status(400).json({ error: err.message });
+       }
+       res.status(200).json(updatedMatch);
+     }
+   );
+ });
 
 
 // Testing Route
