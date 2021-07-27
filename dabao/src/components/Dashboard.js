dfdfdc6119a1614d0d794bfa57929008a953401c
WenHan <wenhantan94@gmail.com>
2021-07-28 00:58:48 +0800

    Dashboard can now delete on both sides, jobs show customers

--- dabao/src/components/Dashboard.js
+++ dabao/src/components/Dashboard.js
@@ -2,6 +2,9 @@ import React, { useEffect, useContext, useState } from "react";
 import Nav from "./Nav";
 import { LoggedContext } from "../App.js";
 import "./Dashboard.css";
+import Chatbox from "./Chatbox";
+import moment from "moment"
+import axios from "axios"
 
 const Dashboard = () => {
   const loggedContext = useContext(LoggedContext);
@@ -9,14 +12,32 @@ const Dashboard = () => {
   const [DBEEdashboard, setDBEEdashboard] = useState([]);
 
   const handleDeleteJob = (id) => {
-    console.log(id);
-
     fetch(`/match/${id}`, {
       method: "DELETE",
     }).then((response) => {
       setDashboard(dashboard.filter((data) => data._id !== id));
     });
   };
+
+  const handleDeleteOrder = (match) => {
+    axios
+    .put(`/match/remove/${match}/${loggedContext.logState._id}`)
+    .then(function (response) {
+      // handle success
+      setDBEEdashboard(DBEEdashboard.filter((data) => data._id !== match))
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  }
+
+  const handleChat = () => {
+    console.log("Chat")
+  }
+
+  const handleEdit = () => {
+    console.log("Edit")
+  }
 
   useEffect(() => {
     fetch(`/match/DBER/${loggedContext?.logState?._id}`, {
@@ -27,8 +48,11 @@ const Dashboard = () => {
       },
     })
       .then((res) => res.json())
-      .then((data) => setDashboard(data));
-  }, [loggedContext.logState._id]);
+      .then((data) => {
+        setDashboard(data)
+        console.log("dashboard", data)
+      });
+  }, [loggedContext?.logState?._id]);
 
   useEffect(() => {
     fetch(`/match/DBEE/${loggedContext?.logState?._id}`, {
@@ -39,31 +63,43 @@ const Dashboard = () => {
       },
     })
       .then((res) => res.json())
-      .then((data) => setDBEEdashboard(data));
-  }, []);
+      .then((data) => {
+        console.log("DBEEdashboard", data)
+        setDBEEdashboard(data)
+      });
+  }, [loggedContext?.logState?._id]);
 
   const activeJobs = dashboard.map((data, index) => {
     return (
-      <div index={index}>
-        <span>Pickup Point: {data.pickupLocation.street}</span>
+      <div key={index}>
+        <span>Pickup Point: {data.pickupLocation.street}, {data.pickupLocation.postCode}</span>
         <br />
-        <span>Pickup Time: {data.timeAtPickUp}</span>
-        <br />
-        <span>Pickup Time: {data._id}</span>
+        <span>Pickup Time: {moment(data.timeAtPickUp).format("LLL")}</span>
         <br />
         <button>Edit</button>
         <button onClick={() => handleDeleteJob(data._id)}>Delete</button>
+        <ul>
+          {data.Orders.map((inner, indexIn) => (
+            <li key={indexIn}>
+              {inner.DBEE.username}
+              <span className="chat">
+                <button onClick={handleChat}>Chat</button>
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   });
 
   const activeOrder = DBEEdashboard.map((data, index) => {
     return (
-      <div index={index}>
-        <span>Pickup Point: {data.pickupLocation.street}</span>
-        <span>Pickup Time: {data.timeAtPickUp}</span>
-        <button>Edit</button>
-        <button>Delete</button>
+      <div key={index}>
+        <span>Pickup Point: {data.pickupLocation.street}, {data.pickupLocation.postCode}</span>
+        <br />
+        <span>Pickup Time: {moment(data.timeAtPickUp).format("LLL")}</span>
+        <br />
+        <button onClick={() => handleDeleteOrder(data._id)}>Delete</button>
       </div>
     );
   });
@@ -73,17 +109,15 @@ const Dashboard = () => {
       <h1>Dashboard</h1>
       <div className="jobsNorders">
         <div className="activeJobs">
-          <h2>Your Jobs</h2>
-          <div>{activeJobs}</div>
+          <h2>Jobs</h2>
+          <div className="jobDetails">{activeJobs}</div>
         </div>
         <div className="activeOrders">
-          <h2>Your Orders</h2>
-          {activeOrder}
+          <h2>Orders</h2>
+          <div className="jobDetails">{activeOrder}</div>
         </div>
       </div>
-      <div>
-        <h1>Chatbox</h1>
-      </div>
+      <Chatbox />
     </Nav>
   );
 };
