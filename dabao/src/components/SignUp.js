8cea04e76365f14f703ec438cfece913d4282752
Darylaw93 <darylaw93@gmail.com>
2021-07-29 12:28:42 +0800

    css changes.

--- dabao/src/components/SignUp.js
+++ dabao/src/components/SignUp.js
@@ -1,101 +1,86 @@
 import * as React from "react";
-import {useState} from "react";
-import { Redirect } from 'react-router'
-import Nav from "./Nav"
+import { useState } from "react";
+import { Redirect } from "react-router";
+import Nav from "./Nav";
 const SignUp = () => {
-  const [isConfirmPwdSame, setIsConfirmPwdSame] = useState(true)
-  const [isSuccess, setIsSuccess] = useState(false)
+  const [isConfirmPwdSame, setIsConfirmPwdSame] = useState(true);
+  const [isSuccess, setIsSuccess] = useState(false);
 
   // need to make sure password and confirm password is same
   // the new user API is post method with /users route
   const handleRegisterUser = (event) => {
     event.preventDefault();
-    if(event.target.password.value !== event.target.confirmPassword.value) {
+    if (event.target.password.value !== event.target.confirmPassword.value) {
       setIsConfirmPwdSame(false);
       return;
     }
-    
+
     fetch("/users", {
       method: "POST",
-      body: JSON.stringify({ 
-        username: event.target.username.value,   
+      body: JSON.stringify({
+        username: event.target.username.value,
         password: event.target.password.value,
-        address: { street: event.target.address.value,
-                    postCode: event.target.postcode.value},
-        contact: {hp: event.target.hp.value,
-                  email: event.target.email.value},
+        address: {
+          street: event.target.address.value,
+          postCode: event.target.postcode.value,
+        },
+        contact: { hp: event.target.hp.value, email: event.target.email.value },
       }),
       headers: {
-      "Content-Type": "application/json",
+        "Content-Type": "application/json",
       },
     })
-    .then((res) => { 
-      console.log(res)
-      if(res.status === 200) {
-        setIsSuccess(true)
-        return res.json()
-      }
-        
-    })
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          setIsSuccess(true);
+          return res.json();
+        }
+      })
       .then((resJson) => {
-        // props.handleAddHoliday(resJson);
-        // console.log("resJson",resJson)
-        
+
       })
       .catch((error) => console.error({ Error: error }));
-
-  }
+  };
 
   return (
-      <Nav>
+    <Nav>
       <h1>Sign Up!</h1>
-    <div className="SignUpForm">
-      <form onSubmit={handleRegisterUser}>
-        <label>
-          Username:
-          <br></br>
-          <input type="text" name="username" placeholder="Username" />
-        </label>
-        <label>
-          Password:
-          <br></br>
-          <input type="password" name="password" placeholder="Password" />
-        </label>
-        <label>
-          Confirm Password:
-          <br></br>
-          <input
-            type="confirmPassword"
-            name="confirmPassword"
-            placeholder="Confirm Password"
-          />
-        </label>
-        <label>
-          Address:
-          <br></br>
-          <input type="text" name="address" placeholder="Address" />
-        </label>
-        <label>
-          Postcode:
-          <br></br>
-          <input type="text" name="postcode" placeholder="postcode" />
-        </label>
-        <label>
-          Email Address:
-          <br></br>
-          <input type="text" name="email" placeholder="Email Address" />
-        </label>
-        <label>
-          Hp:
-          <br></br>
-          <input type="text" name="hp" placeholder="Hp" />
-        </label>
-        <br></br>
-        <input type="submit" value="Register" />
-      </form><br /><br />
-      {!isConfirmPwdSame && <h3>Confirm Password Must Match</h3>}
-      {isSuccess && <Redirect to="/signupsuccess" /> }
-    </div>
+      <div className="SignUpForm">
+        <form onSubmit={handleRegisterUser}>
+          <div className="user-box">
+            <input type="text" name="username" required />
+            <label>Username</label>
+          </div>
+          <div className="user-box">
+            <input type="password" name="password" required />
+            <label>Password</label>
+          </div>
+          <div className="user-box">
+            <input type="password" name="confirmPassword" required />
+            <label>Confirm Password</label>
+          </div>
+          <div className="user-box">
+            <input type="text" name="address" required />
+            <label>Address</label>
+          </div>
+          <div className="user-box">
+            <input type="text" name="postcode" required />
+            <label>Postcode</label>
+          </div>
+          <div className="user-box">
+            <input type="text" name="email" required />
+            <label>Email Address</label>
+          </div>
+          <div className="user-box">
+            <input type="text" name="hp" required />
+            <label>Hp</label>
+          </div>
+          <button className="btstyle">Register</button>
+        </form>
+        {!isConfirmPwdSame && <h3>Confirm Password Must Match</h3>}
+        {isSuccess && <Redirect to="/signupsuccess" />}
+      </div>
     </Nav>
   );
 };
