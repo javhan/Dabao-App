c196a7addb571f0a49cd7946c6824429bda01b91
terencechan <terence.kfc@gmail.com>
2021-07-29 16:23:35 +0800

    Filter DBER based on user address and current pos poscode

--- dabao/src/map.js
+++ dabao/src/map.js
@@ -1,16 +1,40 @@
-// import React, { useContext } from "react";
-// import { LoggedContext } from "./App.js";
+import axios from 'axios'
+require("dotenv").config();
 
 
+const getPostcode = (lat,long, setCurrentPos) => {
+
+    // let postcode;
+    const baseURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
+    // const apiKey = "&key=" + process.env.GOOGLE_MAP_API_KEY;
+    const apiKey = "&key=AIzaSyDzMoyE6vJ1ZE8TcpQeIbUSew8ShVSqmm0"
+    const URL = `${baseURL}${lat},${long}${apiKey}`;
+
+    console.log("URL",URL)
+    axios.get(URL)
+    .then(function (response) {
+      console.log("MAP DATA",response.data);
+      console.log("Poscode6",response.data.results[0].address_components[6]?.long_name)
+      console.log("Poscode5",response.data.results[0].address_components[5]?.long_name)
+      const postcode6 = response.data.results[0].address_components[6]?.long_name;
+      const postcode5 = response.data.results[0].address_components[5]?.long_name;
+      const postcode = postcode6 === undefined ? postcode5:postcode6
+      setCurrentPos({lat,long,postcode})
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+
+}
 
 const setPos = (setCurrentPos) => {
 
     const successCallback = (position) => {
-      // console.log(position);
-    //   const loggedContext = useContext(LoggedContext);
-      console.log(position.coords.latitude)
-      console.log(position.coords.longitude)
-      setCurrentPos({lat:position.coords.latitude, long:position.coords.longitude})
+    //   console.log(position.coords.latitude)
+    //   console.log(position.coords.longitude)
+      const lat = position.coords.latitude;
+      const long = position.coords.longitude;
+      getPostcode(lat,long,setCurrentPos);      
     }
 
     const errorCallback = (error) => {
